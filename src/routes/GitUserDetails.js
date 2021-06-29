5f2307ba3a3d2b9063334f7b7deb446da55f6502
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-29 19:50:40 -0400

    add git user logic

--- src/routes/GitUserDetails.js
+++ src/routes/GitUserDetails.js
@@ -1,16 +1,75 @@
+import axios from 'axios'
+
+import {useEffect, useState} from 'react'
 import {useParams} from 'react-router'
+import {Card, Avatar, Spinner, Flex, Text, Alert, Box} from 'theme-ui'
 
 import Layout from '../components/Layout'
 
 function GitUserDetails() {
   let {id} = useParams()
+  const [status, setStatus] = useState('idle')
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState(null)
 
-  console.log({id})
-  return (
-    <Layout>
-      <h1>User details</h1>
-    </Layout>
-  )
+  const isIdle = status === 'idle'
+  const isPending = status === 'pending'
+  const isRejected = status === 'rejected'
+  const isResolved = status === 'resolved'
+
+  useEffect(() => {
+    if (!id) {
+      return
+    }
+
+    setStatus('pending')
+
+    axios
+      .get(`https://api.github.com/users/${id}`)
+      .then(res => {
+        setUser(res.data)
+        setStatus('resolved')
+      })
+      .catch(err => {
+        setError(err.message)
+        setStatus('rejected')
+      })
+  }, [id])
+
+  if (isIdle) {
+    return 'idle'
+  }
+
+  if (isPending) {
+    return <Spinner />
+  }
+
+  if (isRejected) {
+    return (
+      <Alert variant="error" mb={2}>
+        {error}
+      </Alert>
+    )
+  }
+
+  if (isResolved) {
+    return (
+      <Layout>
+        <h1>User details</h1>
+        <Card>
+          {/* <Flex> */}
+          <Avatar src={user.avatar_url}></Avatar>
+          <Box>
+            <Text>{user.name}</Text>
+          </Box>
+          <Box>
+            <Text>{user.location}</Text>
+          </Box>
+          {/* </Flex> */}
+        </Card>
+      </Layout>
+    )
+  }
 }
 
 export default GitUserDetails
