9e813dcd13b86a24c4aafc2168a6c0752655d943
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-29 19:30:09 -0400

    wip home - show users

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,57 @@
+// import axios from 'axios'
+// import {useEffect} from 'react'
 import Layout from '../components/Layout'
+// import {useState} from 'react'
+import {Container, Box, Flex} from 'theme-ui'
+
+// const apiURL = 'https://api.github.com/users/'
+// const githubUsers = ['GrahamCampbell', 'fabpot', 'y', 'rkh', 'josh']
 
 function Home() {
+  const githubUsers = [
+    'GrahamCampbell',
+    'fabpot',
+    'weierophinney',
+    'rkh',
+    'josh',
+  ]
+
+  // useEffect(() => {
+  //   // I get confuse, i was thinking to show more info about the user in the home page.. :O
+  //   // but is only the name, so I could harcoded the data
+  //   // axios
+  //   //   .all([
+  //   //     axios.get('https://api.github.com/users/GrahamCampbell'),
+  //   //     axios.get('https://api.github.com/users/fabpot'),
+  //   //     axios.get('https://api.github.com/users/weierophinney'),
+  //   //     axios.get('https://api.github.com/users/rkh'),
+  //   //     axios.get('https://api.github.com/users/josh'),
+  //   //   ])
+  //   //   .then(resArr => {
+  //   //   })
+  // }, [])
+
   return (
     <Layout>
-      <div>Home</div>
+      <h1>Top 5 GitHub Users</h1>
+      <Container>
+        <Flex>
+          {githubUsers.map(user => {
+            return (
+              <Box
+                bg="primary"
+                color="white"
+                key={user}
+                p={2}
+                sx={{flex: '1 1 auto', textAlign: 'center'}}
+                m={1}
+              >
+                {user}
+              </Box>
+            )
+          })}
+        </Flex>
+      </Container>
     </Layout>
   )
 }
