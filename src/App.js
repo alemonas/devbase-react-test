567145888e0fecf5888d71a2f538c4438eaeca4b
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-29 18:46:59 -0400

    add main routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css'
 import {ThemeProvider} from 'theme-ui'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import theme from './theme'
+import Home from './routes/Home'
+import GitUserDetails from './routes/GitUserDetails'
+
+function AppRoutes() {
+  return (
+    <>
+      <Switch>
+        <Route path="/git-user-details">
+          <GitUserDetails />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  )
+}
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>Home</div>
+      <Router>
+        <AppRoutes />
+      </Router>
     </ThemeProvider>
   )
 }
---
+++ src/routes/GitUserDetails.js
@@ -0,0 +1,5 @@
+function GitUserDetails() {
+  return <div>git user details page</div>
+}
+
+export default GitUserDetails
